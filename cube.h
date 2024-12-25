ead13f86c971bb5576498a843b479290a5e4a00c
666 <1581859590@qq.com>
2024-12-25 14:27:04 +0800

    ceshi

--- cube.h
+++ cube.h
@@ -1,22 +1,5 @@
 #pragma once
 #include <iostream>
+#include <>
 using namespace std;
 
-class cube
-{
-public:
-	void LL(int L1);
-	int LL1();
-	void WW(int W1);
-	int WW1();
-	void HH(int H1);
-	int HH1();
-	int ss();
-	int vv();
-
-private:
-	int L;//³¤
-	int W;//¿í
-	int H;
-	
-}
--- mianshi.cpp
+++ mianshi.cpp
@@ -4,19 +4,7 @@
 #include <vector>
 using namespace std;
 
-struct ListNode {
-	int val;
-	ListNode* next;
-	ListNode(int x):val(x),next(nullptr){}
-};
-
-ListNode* findmid(ListNode* head) {
-	ListNode* slow = head;
-	ListNode* fast = head;
-
-	while (fast != nullptr && fast->next != nullptr) {
-		slow = slow->next;
-		fast = fast->next->next;
-	}
-	return slow;
+int main()
+{
+	count << "asdfda";
 }
